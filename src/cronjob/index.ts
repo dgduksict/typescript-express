c798486ca2ac0713b8de657cbf8792f0fcb47a0e
duk <B211910809@must.edu.mn>
2025-09-26 16:15:00 +0800

    Add cron job to log every 5 minutes

--- src/cronjob/index.ts
+++ src/cronjob/index.ts
@@ -1,1 +1,8 @@
+import { cron } from "..";
+import logger from "../config/winston";
 
+export async function startCronJobs() {
+  cron.schedule("0 */5 * * * *", async () => {
+    logger.info("Test cronjob.");
+  });
+}

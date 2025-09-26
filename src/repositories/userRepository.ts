4643678a659e432559098b7559401911776605a3
duk <B211910809@must.edu.mn>
2025-09-26 16:26:26 +0800

    Refactor userRepository methods and add new ones

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,64 +1,43 @@
-import { Prisma } from "@prisma/client";
 import { db } from "../utils/db";
 import { User } from "../types/db/types";
-import { Updateable } from "kysely";
+import { Updateable, Insertable } from "kysely";
 
 export const userRepository = {
-  getUserByPhoneNumber: async (phoneNumber: string, prefix: string) => {
-    const user = await db
-      .selectFrom("User")
-      .where("User.telNumber", "=", phoneNumber)
-      .where("User.prefix", "=", prefix)
-      .selectAll()
-      .executeTakeFirst();
-
-    return user;
-  },
-  getUserById: async (id: string) => {
-    const user = await db
-      .selectFrom("User")
-      .where("User.id", "=", id)
-      .selectAll()
-      .executeTakeFirst();
-
-    return user;
-  },
-  create: async (data: Prisma.UserCreateInput) => {
+  create: async (data: Insertable<User>) => {
     const user = await db
       .insertInto("User")
       .values(data)
       .returningAll()
-      .executeTakeFirstOrThrow(() => new Error("Could not create the user."));
+      .executeTakeFirstOrThrow(() => new Error("Couldn't create the user."));
 
     return user;
   },
-  update: async (id: string, data: Prisma.UserCreateInput) => {
+  update: async (id: string, data: Updateable<User>) => {
     const user = await db
       .updateTable("User")
-      .set(data)
-      .where("User.id", "=", id)
       .returningAll()
-      .executeTakeFirstOrThrow(() => new Error("Could not update the user."));
-
-    return user;
-  },
-  updateAsKysely: async (id: string, data: Updateable<User>) => {
-    const user = await db
-      .updateTable("User")
       .set(data)
-      .where("User.id", "=", id)
-      .returningAll()
-      .executeTakeFirstOrThrow(() => new Error("Could not update the user."));
+      .where("id", "=", id)
+      .executeTakeFirstOrThrow(() => new Error("Couldn't update the user."));
 
     return user;
   },
   delete: async (id: string) => {
-    const deletedUser = await db
+    const user = await db
       .deleteFrom("User")
-      .where("User.id", "=", id)
       .returningAll()
-      .executeTakeFirstOrThrow(() => new Error("Could not delete the user."));
+      .where("User.id", "=", id)
+      .executeTakeFirstOrThrow(() => new Error("Couldn't delete the user."));
 
-    return deletedUser;
+    return user;
+  },
+  getById: async (id: string) => {
+    const user = await db
+      .selectFrom("User")
+      .selectAll()
+      .where("User.id", "=", id)
+      .executeTakeFirst();
+
+    return user;
   },
 };

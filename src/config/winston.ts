278e5bda635a58b80f7685ec71524068fe49d0d8
duk <B211910809@must.edu.mn>
2025-09-26 16:18:42 +0800

    Add Winston logger configuration

---
+++ src/config/winston.ts
@@ -0,0 +1,14 @@
+import { config, createLogger, format, transports } from "winston";
+const { combine, timestamp, json, prettyPrint, splat } = format;
+
+const logger = createLogger({
+  level: "info",
+  format: combine(timestamp(), json(), prettyPrint()),
+  transports: [
+    new transports.Console({
+      format: format.combine(json(), timestamp(), splat()),
+    }),
+  ],
+});
+
+export default logger;

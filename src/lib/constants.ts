04bfdd0fd1a24286fece8360d41512c5bed1d8ad
dgduksict <B211910809@must.edu.mn>
2025-09-26 16:36:16 +0800

    updates

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,10 @@
 export const fileSizeLimit = 3145728; //3MB in binary
+
+export const sizeLimitConstants = {
+  fileSizeLimit: 4 * 1024 * 1024, //4MB
+  formDataSizeLimit: "4mb", //4MB
+  jsonSizeLimit: "1mb",
+  formDataSizeLimitUpperCase: "4MB", //4MB
+  jsonSizeLimitUpperCase: "1MB", //1MB
+  IMAGE_MAX_SIZE: 4 * 1024 * 1024, // 4MB in bytes
+};

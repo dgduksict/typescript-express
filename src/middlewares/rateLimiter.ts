163b84b18c63f4871f2b1c4e4bd5c6a1b0d9a33c
duk <B211910809@must.edu.mn>
2025-09-26 16:12:33 +0800

    Update rateLimiter.ts

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -1,13 +1,7 @@
 import { createRateLimiter } from "../lib/createRateLimiter";
 
-export const launchRatelimiter = createRateLimiter({
-  keyPrefix: "ratelimiter-launch",
+export const exampleRateLimiter = createRateLimiter({
+  keyPrefix: "limiter",
   limit: 10,
   window: 3 * 60,
-});
-
-export const collectibleslimiter = createRateLimiter({
-  keyPrefix: "ratelimiter-collectibles",
-  limit: 10,
-  window: 5 * 60,
 });
